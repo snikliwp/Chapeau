235839c0f28da4e957aaedccce0103ca96ce11e7
wilk0146 <wilk0146@algonquinlive.com>
2012-02-17 10:43:36 -0500

    initial load

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'chapeau_phpfogapp_com');
+define('WP_HOME', getenv('WP_HOME'));
+define('WP_SITEURL',getenv('WP_HOME'));
+
+define('DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define('DB_USER', 'wilk0146-29153');
+define('DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Chapeau1');
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql-shared-02.phpfog.com');
+define('DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
