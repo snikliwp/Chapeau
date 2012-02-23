540883594d51611914e6313077c89a7dd72646ed
snikliwp <wilk0146@algonquinlive.com>
2012-02-23 15:58:05 -0500

    first load

---
+++ wp-content/themes/chapeau/template-goals-page.php
@@ -0,0 +1,30 @@
+<?php  
+/*
+Template Name: Goals and Objectives page
+*/
+?>
+<?php 
+	get_header(); 
+	the_post();
+	$goals_post = $post;
+?>
+
+<p>This is the Goals and Objectives page</p>
+<ul>
+	<li>to promote and reward good agricultural practice.</li>
+	<li>to create opportunities for members to meet and exchange ideas.</li>
+	<li>to provide educational and training opportunities for members.</li>
+	<li>to advocate on behalf of farmers.</li>
+	<li>to contribute to community development through activities such as a Farmer’s Market, a Community Garden, an annual Calendar</li>
+	<li>to increase Society membership</li>
+</ul>
+
+<?php 
+setup_postdata($goals_post);
+the_content();
+ ?>
+
+
+
+<?php get_footer(); ?>
+
---
+++ wp-content/themes/chapeau/template-mission-page.php
@@ -0,0 +1,23 @@
+<?php  
+/*
+Template Name: Mission Statement page
+*/
+?>
+<?php 
+	get_header(); 
+	the_post();
+	$mission_post = $post;
+?>
+
+<p>This is the Mission Statement page</p>
+<p>The Society works to strengthen the region’s agriculture through advocacy, education, community development, communication and fundraising while encouraging best practices and respect for the land.</p>
+
+<?php 
+setup_postdata($mission_post);
+the_content();
+ ?>
+
+
+
+<?php get_footer(); ?>
+
