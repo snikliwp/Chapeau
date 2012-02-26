a3bf419a8900a2c9d73c9eecdfb040410d257aea
snikliwp <wilk0146@algonquinlive.com>
2012-02-26 16:23:15 -0500

    couple of fixes

--- wp-content/themes/chapeau/footer.php
+++ wp-content/themes/chapeau/footer.php
@@ -2,7 +2,7 @@
 <div id="haybail"> </div>
 
 <footer class="bottom">
-	<div class="footercontent">Societe D'Agriculture de <span>Chapeau</span> Agricultural Sosciety</div>
+	<div class="footercontent">Societe D'Agriculture de <span>Chapeau</span> Agricultural Society</div>
 </footer>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
