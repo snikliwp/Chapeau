16c79d119f4018d4fa7b4345857beb67835ccb9c
vank <roger.van.koughnett@gmail.com>
2012-03-08 17:39:48 -0500

    re-formatted

--- wp-content/themes/chapeau/template-where-page.php
+++ wp-content/themes/chapeau/template-where-page.php
@@ -10,27 +10,12 @@ Template Name: where page
 ?>
 <article>
 	<div class="middle">
-
-<p>This is the Where and When page</p>
-
-<h2>Annual Schedule</h2>
-<p>TBA</p>
-
-<h2>Location</h2>
-<p>Harrington Hall in Chapeau, Quebec. (see map)</p>
-<a href="#">Location Map</a>
-
-<h2>Hours</h2>
-<p>TBA</p>
-
-<h2>Season</h2>
-<p>TBA</p>
-</div>
 <?php 
 setup_postdata($where_post);
 the_content();
  ?>
 
+</div>
 
 
 <?php get_footer(); ?>
