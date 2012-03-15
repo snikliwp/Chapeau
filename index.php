e933cc2d203a6e07752134d0df329f289c9cc9be
snikliwp <wilk0146@algonquinlive.com>
2012-03-15 14:02:57 -0400

    fixed index file

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?php get_header(); ?>
+<?php
+/**
+* Front to the WordPress application. This file doesn't do anything, but loads
+* wp-blog-header.php which does and tells WordPress to load the theme.
+*
+* @package WordPress
+*/
 
-<?php 
-//	if (have_posts()):
-//		while(have_posts()):
-			the_post();
+/**
+* Tells WordPress to load the WordPress theme and output it.
+*
+* @var bool
+*/
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
 ?>
-
-
-
-<?php get_footer(); ?>
-
