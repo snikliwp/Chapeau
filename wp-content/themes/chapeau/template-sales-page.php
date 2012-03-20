a3d5b341f07797725c365f9920005b4f2ab6ae6e
snikliwp <wilk0146@algonquinlive.com>
2012-03-20 15:49:25 -0400

    working on sales today

--- wp-content/themes/chapeau/template-sales-page.php
+++ wp-content/themes/chapeau/template-sales-page.php
@@ -10,15 +10,50 @@ Template Name: sales page
 ?>
 <article>
 	<div class="middle">
-
-<p>This is the sales page</p>
-</div>
+		<p>This is the sales page</p>
+		<?php 
+			$news = get_posts(array(
+				"post_type" => "advertisment", // The kind of post that we want to display
+				"numberposts" => -1,
+				));
+				
+			foreach ($news as $post) :
+				setup_postdata($post);
+				$files = get_posts(array(
+					'post_type' => 'attachment'
+					, 'numberposts' => 1
+					, 'post_parent' => $post->ID
+				));
+		?>
+		
+		<article class="news">
+			<div class="ad">
+				<div class="adtitle">
+					<h2 itemprop="name" class = "product-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				</div><!-- end class adtitle -->
+				<div class="adname">
+					<?php more_fields('ad-name', '<li><span>', '</span></li>'); ?>
+				</div> <!-- end class adname -->
+				<div class="adcat">
+					<?php more_fields('ad-cat', '<li><span>', '</span></li>'); ?>
+				</div><!-- end class adtxt -->
+				<div class="adtxt">
+					<?php more_fields('ad-text', '<li><span>', '</span></li>'); ?>
+				</div> <!-- end class adtxt -->
+			</div> <!--end of class ad-->
+		<br>
+		<?php endforeach; ?>
+		
+		</article> <!--end of article class news-->
+	</div> <!--end of middle ad-->
 <?php 
-setup_postdata($sales_post);
-the_content();
- ?>
+ setup_postdata($sales_post);
+ the_content();
+ get_footer(); 
+?>
+
+<!-- more fields version 2.0.5 for edumedia
+-->
 
 
-
-<?php get_footer(); ?>
 
