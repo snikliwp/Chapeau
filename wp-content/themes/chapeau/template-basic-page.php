98d3d52548741d53f58901ba5e0a26a5748a9468
unknown <vank0026@WT308-16.mmserver.ottawa.ad.algonquincollege.com>
2012-03-29 16:13:26 -0400

    removed excess templates, condensed into basci

--- wp-content/themes/chapeau/template-basic-page.php
+++ wp-content/themes/chapeau/template-basic-page.php
@@ -1,24 +1,27 @@
 <?php  
 /*
-Template Name: Membership page
+Template Name: Basic Template
 */
 ?>
 <?php 
 	get_header(); 
 	the_post();
-	$member_post = $post;
 ?>
 
 <article>
+
 	<div class="middle-top">
+        <h1><?php the_title(); ?><h1>
     </div>
 
 	<div class="middle">
-<?php 
-setup_postdata($member_post);
-the_content();
- ?>
-	</div>
+    
+	<?php 
+    setup_postdata($post);
+    the_content();
+     ?>
+     
+    </div>
 
 	<div class="middle-bottom">
     </div>
