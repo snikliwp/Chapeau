9c6f1137d27447012e5f851f68976ecb33bedb61
snikliwp <wilk0146@algonquinlive.com>
2012-03-29 18:42:23 -0400

    changes made

--- wp-content/themes/chapeau/functions.php
+++ wp-content/themes/chapeau/functions.php
@@ -44,24 +44,24 @@ function url () {
    /* we only want this function to be called when Wordpress is ready */
    // the add_action function allows us to call our own function at a specific time
 /**************************************************************************************/
-add_action("init", "create_events_post_type");
-
-function create_events_post_type() {
-	
-	$labels = array(
-		"name" => "Events", // plural version of the post name
-		"singular_name" => "Event",
-  		"add_new_item" => "Add New Event" // Changes title from add new post to add new contact
-		);
-				
-	register_post_type("event", array(
-		"labels" => $labels,
-		"public" => true,
-		"supports" => array("title", "editor", "custom-fields"), // default is array("title", "editor")
-		"taxonomies" => array('category', 'post_tag')
-		));
-		
-}
+//add_action("init", "create_events_post_type");
+//
+//function create_events_post_type() {
+//	
+//	$labels = array(
+//		"name" => "Events", // plural version of the post name
+//		"singular_name" => "Event",
+//  		"add_new_item" => "Add New Event" // Changes title from add new post to add new contact
+//		);
+//				
+//	register_post_type("event", array(
+//		"labels" => $labels,
+//		"public" => true,
+//		"supports" => array("title", "editor", "custom-fields"), // default is array("title", "editor")
+//		"taxonomies" => array('category', 'post_tag')
+//		));
+//		
+//}
 /**************************************************************************************/
 add_action("init", "create_post_type");
 
