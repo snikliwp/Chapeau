810ae07213da8ea0461d43aaa743b715d3dde7bc
snikliwp <wilk0146@algonquinlive.com>
2012-04-06 15:07:49 -0400

    Added the post instructions

--- wp-content/themes/chapeau/index.php
+++ wp-content/themes/chapeau/index.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 
 <?php 
-//	if (have_posts()):
-//		while(have_posts()):
+	if (have_posts()):
+		while(have_posts()):
 			the_post();
+		endwhile;
 ?>
 
 
 
+<?php endif; ?>
 <?php get_footer(); ?>
 
