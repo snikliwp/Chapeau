4a73c640d110fde7613727e7468f0400a1186bff
unknown <vank0026@WT229-22W.mmserver.ottawa.ad.algonquincollege.com>
2012-04-10 15:31:05 -0400

    modifeid for IE6,7,8 compatability

--- wp-content/themes/chapeau/header.php
+++ wp-content/themes/chapeau/header.php
@@ -4,10 +4,17 @@
   <meta charset="utf-8">
   <title><?php wp_title(''); ?>Chapeau Agricultural Society</title>
   <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/ie6.css" media="screen" type="text/css" />
+    <![endif]-->
+    
   <script src="<?php sd(); ?>/js/modernizr.min.js"></script>
 	<?php wp_head() ?>
+    
+
 </head>
-<body>
+<body class="clearfix">
 
 <header class="top">
 	<h1 class="topLinks"><a href="#">Français</a><a href="#">English</a></h1>
